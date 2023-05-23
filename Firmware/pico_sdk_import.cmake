d1b205462744a6aa0aad53298c6f3accb2295f03
ytsurui <tsuruiy@gmail.com>
2023-05-24 01:07:27 +0900

    環境変数のPICO_SDK_PATHを強制的に読み込むように変更

--- Firmware/pico_sdk_import.cmake
+++ Firmware/pico_sdk_import.cmake
@@ -3,6 +3,9 @@
 # This can be dropped into an external project to help locate this SDK
 # It should be include()ed prior to project()
 
+set(PICO_SDK_PATH $ENV{PICO_SDK_PATH})
+message("Using PICO_SDK_PATH from environment ('${PICO_SDK_PATH}')")
+
 if (DEFINED ENV{PICO_SDK_PATH} AND (NOT PICO_SDK_PATH))
     set(PICO_SDK_PATH $ENV{PICO_SDK_PATH})
     message("Using PICO_SDK_PATH from environment ('${PICO_SDK_PATH}')")
